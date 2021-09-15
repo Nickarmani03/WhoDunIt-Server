a9066826b3952f79875f4a61715c7b432d2b3729
Nickarmani03 <nickarmani03@gmail.com>
2021-09-15 09:10:03 -0500

    updated register

--- whodunitapi/views/auth.py
+++ whodunitapi/views/auth.py
@@ -5,14 +5,11 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import status
-from django.views.decorators.csrf import csrf_exempt
 from whodunitapi.models import Player
-from django.http import HttpResponse
-import json
+
 
 @api_view(["POST"])
 @permission_classes([AllowAny])
-@csrf_exempt
 
 def login_user(request):
     '''Handles the authentication of a player
@@ -20,33 +17,27 @@ def login_user(request):
       request -- The full HTTP request object
     '''
 
-    req_body = json.loads(request.body.decode())
+    # Use the built-in authenticate method to verify
+    username = request.data['username']
+    password = request.data['password']
 
-    # If the request is a HTTP POST, try to pull out the relevant information.
-    if request.method == 'POST':
+    authenticated_user = authenticate(username=username, password=password)
+    data = {}
 
-        # Use the built-in authenticate method to verify
-        username = req_body['username']
-        password = req_body['password']
-        authenticated_user = authenticate(username=username, password=password)
-        data = {}
+    # If authentication was successful, respond with their token
+    if authenticated_user is not None:
+        token = Token.objects.get(user=authenticated_user)
+        data = {
+            'valid': True,
+            'token': token.key
+            }
+    else:
+        data = {'valid': False}
 
-        # If authentication was successful, respond with their token
-        if authenticated_user is not None:
-            token = Token.objects.get(user=authenticated_user)
-            data = json.dumps({"valid": True, "token": token.key})
-
-            return HttpResponse(data, content_type='application/json')
-
-        else:
-            # Bad login details were provided. So we can't log the user in.
-            data = json.dumps({"valid": False})
-
-            return HttpResponse(data, content_type='application/json')
+        return Response(data)
 
 @api_view(["POST"])
 @permission_classes([AllowAny])
-@csrf_exempt
 
 def register_user(request):
     '''Handles the creation of a new player for authentication
@@ -54,24 +45,23 @@ def register_user(request):
       request -- The full HTTP request object
     '''
 
-    # Load the JSON string of the request body into a dict
-    req_body = json.loads(request.body.decode())
 
     # Create a new user by invoking the `create_user` helper method
     # on Django's built-in User model
     new_user = User.objects.create_user(
-        username=req_body['username'],
-        email=req_body['email'],
-        password=req_body['password'],
-        first_name=req_body['first_name'],
-        last_name=req_body['last_name'],
-        profile_image_url=req_body['profile_image_url']
+        username=request.data['username'],
+        email=request.data['email'],
+        password=request.data['password'],
+        first_name=request.data['first_name'],
+        last_name=request.data['last_name']
     )
 
     # Now save the extra info in the whodunitapi_player table
     player = Player.objects.create(
-        bio=req_body['bio'],
-        user=new_user
+        user=new_user,
+        bio=request.data['bio'],
+        profile_image_url=request.data['profile_image_url'],
+        
     )
 
     # Commit the user to the database by saving it
@@ -81,5 +71,7 @@ def register_user(request):
     token = Token.objects.create(user=new_user)
 
     # Return the token to the client
-    data = json.dumps({"token": token.key})
+    data = {
+        'token': token.key
+    }
     return Response(data, status=status.HTTP_201_CREATED)
