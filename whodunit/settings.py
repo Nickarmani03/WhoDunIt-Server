877f4caa447c82672447fbb83995db2374d88a07
Nickarmani03 <nickarmani03@gmail.com>
2021-09-16 11:42:25 -0500

    added some fixes to get the profile views

--- whodunit/settings.py
+++ whodunit/settings.py
@@ -15,8 +15,8 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
-MEDIA_URL= "/media/"
+# MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
+# MEDIA_URL= "/media/"
 
 
 # Quick-start development settings - unsuitable for production
@@ -74,7 +74,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.template.context_processors.media',
+    # 'django.template.context_processors.media',
 ]
 
 ROOT_URLCONF = 'whodunit.urls'
